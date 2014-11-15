154b70ea579692efc3526e4a72820a90379e4280
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2014-11-14 15:54:52 -1000

    adding metrics

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reload(sys).setdefaultencoding('Utf-8')
 
 setup(
     name='django-gipsy',
-    version='0.0.2',
+    version='0.0.3',
     author='Guillaume Pousseo',
     author_email='guillaumepousseo@revsquare.com',
     description='A set of fancy tools for django.',
