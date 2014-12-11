5919260b82ae4135616477a69c6210d17fa70fd0
tomasz.roszko <tomaszroszko@gmail.com>
2014-12-11 15:01:17 +0100

    Add link in toolbar that is showing up when we are on object detail page and give a link to admin page where object can be edited

--- gipsy/toolbar/settings.py
+++ gipsy/toolbar/settings.py
@@ -6,3 +6,15 @@ def reformat_settings():
     if not 'django.core.context_processors.request' in settings.TEMPLATE_CONTEXT_PROCESSORS:
         settings.TEMPLATE_CONTEXT_PROCESSORS = \
             settings.TEMPLATE_CONTEXT_PROCESSORS + ('django.core.context_processors.request',)
+
+
+LINK_ENABLED = getattr(settings, 'GIPSY_TOOLBAR_LINK_ENABLED', True)
+LINK_CONTEXT_NAME = getattr(
+    settings, 'GIPSY_TOOLBAR_LINK_CONTEXT_NAME', 'object')
+# GIPSY_TOOLBAR_LINK_INCLUDED_MODELS
+# define which models links to admin edit page. If `None` all of the models
+# Example:
+# GIPSY_TOOLBAR_LINK_INCLUDED_MODELS = ('auth.User',)
+
+LINK_INCLUDED_MODELS = getattr(settings, 'GIPSY_TOOLBAR_LINK_INCLUDED_MODELS',
+    None)
--- gipsy/toolbar/templatetags/gipsy_toolbar.py
+++ gipsy/toolbar/templatetags/gipsy_toolbar.py
@@ -1,8 +1,9 @@
 from django import template
 from django.conf import settings
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, reverse
 
 from gipsy.toolbar.models import GipsyToolbarMenu
+from gipsy.toolbar.settings import LINK_CONTEXT_NAME, LINK_INCLUDED_MODELS
 
 
 register = template.Library()
@@ -30,3 +31,29 @@ def gipsy_toolbar(context, *args, **kwargs):
 
     context['is_admin'] = is_admin(context)
     return context
+
+
+@register.assignment_tag(takes_context=True)
+def gipsy_toolbar_link(context):
+    """
+    :param context: django template context
+    :return: link to admin edit page for founded object
+    """
+    obj = context.get(LINK_CONTEXT_NAME, None)
+    if obj is None:
+        return None
+
+    app_label = obj.__class__._meta.app_label
+    model_name= obj.__class__._meta.object_name
+    app_ident = u'%s.%s' % (app_label, model_name)
+
+    if LINK_INCLUDED_MODELS is None or app_ident in LINK_INCLUDED_MODELS:
+        link = reverse('admin:%s_%s_change' % (
+            app_label.lower(), model_name.lower()), args=(obj.pk,))
+        return link
+    else:
+        return None
+
+
+
+
