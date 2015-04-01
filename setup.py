d7a609d3340aa0a904ddf5c7a5b1a3eb73e8354d
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2015-04-01 11:59:33 -1000

    fixing active in submenu

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reload(sys).setdefaultencoding('Utf-8')
 
 setup(
     name='django-gipsy',
-    version='1.1.0',
+    version='1.1.1',
     author='Guillaume Pousseo',
     author_email='guillaumepousseo@revsquare.com',
     description='A set of fancy tools for django.',
