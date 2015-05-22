63567f8741d50b62ee7d012b9477a9ab01b6aa0e
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2015-05-22 12:18:03 -1000

    Improving the general display of buttons on top

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reload(sys).setdefaultencoding('Utf-8')
 
 setup(
     name='django-gipsy',
-    version='1.1.2',
+    version='1.1.3',
     author='Guillaume Pousseo',
     author_email='guillaumepousseo@revsquare.com',
     description='A set of fancy tools for django.',
