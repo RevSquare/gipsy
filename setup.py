29feabd3b96e62fbb5a0ab655ca7a61615b7188f
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2015-07-24 15:52:15 -1000

    releasing version 1.1.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='django-gipsy',
-    version='1.1.7',
+    version='1.1.8',
     author='Guillaume Pousseo',
     author_email='guillaumepousseo@revsquare.com',
     description='A set of fancy tools for django.',
