fb9f12eff6763238e876ecf3bcb4b5cac7d4aa74
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2016-10-24 12:38:57 +0200

    Version 1.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='django-gipsy',
-    version='1.3',
+    version='1.4',
     author='Guillaume Pousseo',
     author_email='guillaumepousseo@revsquare.com',
     description='A set of fancy tools for django.',
